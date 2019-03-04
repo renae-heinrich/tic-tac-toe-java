96f26fa8a45248bdcafdb24ef4525c7934ff5ab7
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-03-05 10:57:42 +1100

    turn count not adding to itself after every turn

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -5,6 +5,7 @@ import java.util.List;
 public class Board {
 
     private final List<List<Character>> character;
+//    private static boolean inPlay = true;
 
     public Board() {
 
@@ -32,7 +33,7 @@ public class Board {
             for (int column = 0; column < 3; column++) {
                 Character c = character.get(column).get(row);
                 sb.append(c);
-                if(column == 2 && row ==2){
+                if(column == 2 && row == 2){
                     break;
                 } else if(column == 2){
                     sb.append('\n');
@@ -44,14 +45,27 @@ public class Board {
     }
 
     public void setPosition(int x, int y) {
-        int row = x -1;
+
+        int row = x - 1;
         int col = y - 1;
 
-        if(character.get(row).get(col).equals('.')) {
-            character.get(row).set((col), 'X');
-        } else{System.out.println("Oh no, a piece is already at this place! Try again...");}
+            if (character.get(col).get(row).equals('.')) {
+                int turnCount = 0;
+                if(turnCount % 2 == 0 ){
+                    character.get(col).set((row), 'X');
+                    turnCount++;
+                } else {
+                    character.get(col).set((row), 'O');
+                    turnCount++;
+                }
+            } else {
+                System.out.println("Oh no, a piece is already at this place! Try again...");
+            }
+
 
     }
+
+    //need to implement turnCounter so X changes to O when turnCount is odd
 
 
 
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -41,7 +41,7 @@ public class GameTest {
         String result = board.showBoard();
 
         Assert.assertEquals("X . .\n. . .\n. . .", result);
-    }
+     }
 
     @Test
     public void whenPlayerForfeitsOtherPlayerWins() {
@@ -49,22 +49,69 @@ public class GameTest {
 
     @Test
     public void whenPlayerGetsThreeInARowHorizontallyPlayerWins() {
+        Game game = new Game();
+        game.takeTurn(1,1);
+        game.takeTurn(1,2);
+        game.takeTurn(1,3);
+        Board board = game.getBoard();
+        String result = board.showBoard();
+
+
+        Assert.assertEquals("X X X\n. . .\n. . .", result);
     }
 
     @Test
     public void  whenPlayerGetsThreeInARowVerticallyPlayerWins() {
+        Game game = new Game();
+        game.takeTurn(1, 1);
+        game.takeTurn(2, 1);
+        game.takeTurn(3, 1);
+        Board board = game.getBoard();
+        String result = board.showBoard();
+
+        Assert.assertEquals("X . .\nX . .\nX . .", result);
     }
 
     @Test
     public void  whenPlayerGetsThreeInARowDiagonallyBottomLeftToTopRightPlayerWins() {
+        Game game = new Game();
+        game.takeTurn(3, 1);
+        game.takeTurn(2, 2);
+        game.takeTurn(1, 3);
+        Board board = game.getBoard();
+        String result = board.showBoard();
+
+        Assert.assertEquals(". . X\n. X .\nX . .", result);
     }
 
     @Test
     public void whenPlayerGetsThreeInARowDiagonallyTopLeftToBottomRightPlayerWins() {
+        Game game = new Game();
+        game.takeTurn(1, 1);
+        game.takeTurn(2, 2);
+        game.takeTurn(3,3);
+        Board board = game.getBoard();
+        String result = board.showBoard();
+
+        Assert.assertEquals("X . .\n. X .\n. . X", result);
     }
 
     @Test
     public void whenAllPositionsFilledButNoPlayerWinsItsADraw() {
+        Game game = new Game();
+        game.takeTurn(1, 1);
+        game.takeTurn(1, 2);
+        game.takeTurn(1, 3);
+        game.takeTurn(2,1);
+        game.takeTurn(2, 2);
+        game.takeTurn(2, 3);
+        game.takeTurn(3, 1);
+        game.takeTurn(3, 2);
+        game.takeTurn(3, 3);
+        Board board = game.getBoard();
+        String result = board.showBoard();
+
+        Assert.assertEquals("X X X\nX X X\nX X X", result);
     }
 
 }
