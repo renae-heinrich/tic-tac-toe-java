b2ae3883f2d05eee33448e16e5c383bc077bfd65
Renae Heinrich <renae.heinrich@c02wx3xejg5j.myob.myobcorp.net>
2019-03-05 16:36:24 +1100

    trying to work out user input validations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,10 +16,28 @@ public class Main {
             Character token = playerOneTurn ? playerOneToken : playerTwoToken;
             boolean turnPlayed;
             do {
-                System.out.println("Enter X:");
-                int x = scanner.nextInt();
-                System.out.println("Enter Y:");
-                int y = scanner.nextInt();
+                System.out.print("enter a coord x,y to place your O or enter 'q' to give up: ");
+                String userInput  = scanner.next();
+                String[] userToken = userInput.split(",");
+                // if userToken array length is one, assuming user is typing q
+                if(userToken.length == 1){
+                    for(String input : userToken){
+                        // checking if input is q, if its not: need to prompt them to start again
+                        if(input.equalsIgnoreCase("q")){
+                            System.out.println("Give up!");
+                            break;
+
+                        } else {break;}
+                    }
+                }else {
+                    for(String input : userToken){
+                        int x = Integer.parseInt(input.valueOf(0));
+                        int y = Integer.parseInt(input.valueOf(1));
+                        //make sure input[i] is a number between 1, 3 and assign to x and y respectively
+                    } return x, y;
+                }
+                //need to return the values of x y but says dont have return type
+
                 turnPlayed = game.takeTurn(x, y, token);
                 if (!turnPlayed) {
                     System.out.println("Position occupied");
